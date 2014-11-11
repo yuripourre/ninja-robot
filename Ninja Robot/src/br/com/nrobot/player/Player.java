e38edc937d3fcb86e7827ff4f3d98734e12bfce5
Yuri Pourre <yuriamayuri@gmail.com>
2014-11-10 23:39:10 -0200

    Add hitbox

--- Ninja Robot/src/br/com/nrobot/player/Player.java
+++ Ninja Robot/src/br/com/nrobot/player/Player.java
@@ -3,9 +3,11 @@ package br.com.nrobot.player;
 import br.com.etyllica.core.event.KeyEvent;
 import br.com.etyllica.core.graphics.Graphic;
 import br.com.etyllica.layer.AnimatedLayer;
+import br.com.etyllica.layer.GeometricLayer;
+import br.com.nrobot.fallen.Fallen;
 
 public class Player {
-
+	
 	private AnimatedLayer layer;
 	
 	private boolean walkRight = false;
@@ -15,13 +17,18 @@ public class Player {
 	
 	private int speed = 10;
 	
+	private GeometricLayer hitbox;
+	
 	public Player(int x, int y) {
 		layer = new AnimatedLayer(x, y, 64, 64, "robot.png");
 		layer.setFrames(4);
 		layer.setSpeed(100);
+		
+		hitbox = new GeometricLayer(x, y, layer.getTileW()-16*2, layer.getTileH()-30);
 	}
 	
 	public void update(long now) {
+		
 		if(walkRight||walkLeft) {
 			
 			if(turnRight) {
@@ -34,10 +41,24 @@ public class Player {
 			
 			layer.animate(now);
 		}
+		
+		updateHitbox();
+	}
+	
+	private void updateHitbox() {
+		hitbox.setCoordinates(16+layer.getX(), layer.getY()+4);
 	}
 
 	public void draw(Graphic g) {
 		layer.draw(g);
+		
+		g.setAlpha(60);
+		g.fillRect(hitbox);
+		g.setAlpha(100);
+	}
+	
+	public boolean colide(Fallen fallen) {
+		return hitbox.colideRect(fallen);
 	}
 
 	public void handleEvent(KeyEvent event) {
@@ -61,7 +82,7 @@ public class Player {
 		
 		if(event.isKeyUp(KeyEvent.TSK_SETA_ESQUERDA)) {
 			walkLeft = false;
-		}	
+		}
 	}
 	
 }
