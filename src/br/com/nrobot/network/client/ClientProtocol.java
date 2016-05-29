5d701022bcd99c0b6982f8bdfc28040c5933391d
André Luiz Pardal <alpardal@gmail.com>
2016-05-29 12:47:42 -0300

    Remove unused methods in ClientProtocol, plus formatting

--- src/br/com/nrobot/network/client/ClientProtocol.java
+++ src/br/com/nrobot/network/client/ClientProtocol.java
@@ -1,14 +1,10 @@
 package br.com.nrobot.network.client;
 
-import br.com.etyllica.core.event.KeyEvent;
-import br.com.etyllica.core.event.PointerEvent;
 import br.com.midnight.model.Peer;
 import br.com.midnight.protocol.common.StringClientProtocol;
 import br.com.nrobot.config.Config;
 
 public class ClientProtocol extends StringClientProtocol {
-
-	private ClientListener listener;
 
 	public static final String PREFIX_NINJA_ROBOT = "nr";
 
@@ -40,113 +36,89 @@ public class ClientProtocol extends StringClientProtocol {
 	public static final String SPRITE_BLUE = "b";
 	public static final String SPRITE_DARK = "d";
 
+	private ClientListener listener;
+
 	public ClientProtocol(ClientListener listener) {
 		super(PREFIX_NINJA_ROBOT);
 		this.listener = listener;
 	}
 
 	public void sendPressKey(String key) {
-		sendTCP(PREFIX_COMMAND+" "+STATE_PRESS+" "+key);
+		sendTCP(PREFIX_COMMAND + " " + STATE_PRESS + " " + key);
 	}
 
 	public void sendReleaseKey(String key) {
-		sendTCP(PREFIX_COMMAND+" "+STATE_RELEASE+" "+key);
+		sendTCP(PREFIX_COMMAND + " " + STATE_RELEASE + " " + key);
 	}
 
 	public void sendName(String name) {
-		sendTCP(PREFIX_CONFIG+" "+CONFIG_NAME+" "+name);
+		sendTCP(PREFIX_CONFIG + " " + CONFIG_NAME + " " + name);
 	}
 
 	public void sendSprite(String sprite) {
-		sendTCP(PREFIX_CONFIG+" "+CONFIG_SPRITE+" "+sprite);
+		sendTCP(PREFIX_CONFIG + " " + CONFIG_SPRITE + " " + sprite);
 	}
 
 	public void sendStart() {
-		sendTCP(PREFIX_CONFIG+" "+CONFIG_START);
+		sendTCP(PREFIX_CONFIG + " " + CONFIG_START);
 	}
 
 	public void sendRessurrect() {
-		sendTCP(PREFIX_CHEAT_CODE+" "+CHEAT_RESSURRECT);
-	}
-
-	public void sendMessage(String message) {
-		sendTCP(PREFIX_MESSAGE+" "+message);
-	}
-
-	public void sendKeyEvent(KeyEvent event) {
-		String message = event.getState().toString()
-				+" "+Integer.toString(event.getKey());
-
-		sendTCP(message);
-	}
-
-	public void sendPointerEvent(PointerEvent event) {
-		String message = event.getState().toString()
-				+" "+Integer.toString(event.getX())
-				+" "+Integer.toString(event.getY());
-
-		sendTCP(message);
+		sendTCP(PREFIX_CHEAT_CODE + " " + CHEAT_RESSURRECT);
 	}
 
 	@Override
 	public void receiveTCP(Peer peer, String msg) {
 
-		if(msg.startsWith(PREFIX_INIT)) {
+		if (msg.startsWith(PREFIX_INIT)) {
 			//HandShake message
-			String crop = msg.substring((PREFIX_INIT+" ").length());
+			String crop = msg.substring((PREFIX_INIT + " ").length());
 			String[] ids = crop.split(" ");
 			listener.init(ids);
 
 			Config config = listener.getConfig();
 			sendName(config.getName());
-		} else if(msg.startsWith(PREFIX_JOIN)) {
+		} else if (msg.startsWith(PREFIX_JOIN)) {
 			String id = msg.split(" ")[1];
 			String name = msg.split(" ")[2];
 			listener.joinedClient(id, name);
-		} else if(msg.startsWith(PREFIX_EXIT)) {
+		} else if (msg.startsWith(PREFIX_EXIT)) {
 			String id = msg.split(" ")[1];
 			listener.exitClient(id);
-		} else if(msg.startsWith(PREFIX_POSITIONS)) {
-			String crop = msg.substring((PREFIX_POSITIONS+" ").length());
+		} else if (msg.startsWith(PREFIX_POSITIONS)) {
+			String crop = msg.substring((PREFIX_POSITIONS + " ").length());
 			System.out.println(crop);
 			listener.updatePositions(crop);
-		} else if(msg.startsWith(PREFIX_CONFIG)) {
-			String crop = msg.substring((PREFIX_CONFIG+" ").length());
+		} else if (msg.startsWith(PREFIX_CONFIG)) {
+			String crop = msg.substring((PREFIX_CONFIG + " ").length());
 
 			String[] parts = crop.split(" ");
 			String id = parts[0];
 			String config = parts[1];
 			String value = parts[2];
 
-			if(CONFIG_NAME.equals(config)) {
+			if (CONFIG_NAME.equals(config)) {
 				listener.updateName(id, value);
-			} else if(CONFIG_SPRITE.equals(config)) {
+			} else if (CONFIG_SPRITE.equals(config)) {
 				listener.updateSprite(id, value);
-			} else if(CONFIG_READY.equals(config)) {
+			} else if (CONFIG_READY.equals(config)) {
 				listener.updateReady(id);
-			} else if(CONFIG_START.equals(config)) {
+			} else if (CONFIG_START.equals(config)) {
 				listener.startGame();
 			}
 
-		} else if(msg.startsWith(PREFIX_MESSAGE)) {
+		} else if (msg.startsWith(PREFIX_MESSAGE)) {
 			String id = msg.split(" ")[0];
-			String message = msg.substring((PREFIX_MESSAGE+" ").length());
+			String message = msg.substring((PREFIX_MESSAGE + " ").length());
 			listener.receiveMessage(id, message);
 		}
 	}
 
 	@Override
 	public void receiveUDP(Peer peer, String msg) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public ClientListener getListener() {
-		return listener;
 	}
 
 	public void setListener(ClientListener listener) {
 		this.listener = listener;
 	}
-
 }
