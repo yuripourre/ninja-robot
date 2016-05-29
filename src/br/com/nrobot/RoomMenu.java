9e9ac3905f79555beee0017708fbe342cc195b9f
André Luiz Pardal <alpardal@gmail.com>
2016-05-29 12:38:48 -0300

    Merge NRobotClient into NinjaRobotClient

--- src/br/com/nrobot/RoomMenu.java
+++ src/br/com/nrobot/RoomMenu.java
@@ -17,13 +17,13 @@ import br.com.nrobot.ui.NRButton;
 public class RoomMenu extends Application {
 
 	private GameMode mode;
-	
+
 	private ImageLayer background;
 	private ImageLayer logo;
-	
+
 	private NRButton createButton;
 	private NRButton joinButton;
-		
+
 	public RoomMenu(int w, int h) {
 		super(w, h);
 	}
@@ -32,62 +32,58 @@ public class RoomMenu extends Application {
 	public void load() {
 		mode = (GameMode) session.get(MainMenu.PARAM_MODE);
 		background = new ImageLayer("background.png");
-		
+
 		logo = new ImageLayer(0, 70, "logo.png");
 		logo.centralizeX(background);
-		
+
 		createButton = new NRButton(232, 300, "create_room.png");
-		
+
 		joinButton = new NRButton(232, 400, "join_room.png");
-		
+
 		loading = 100;
 	}
-	
+
 	@Override
 	public void draw(Graphics g) {
 		background.draw(g);
-		
+
 		logo.draw(g);
-		
+
 		createButton.draw(g);
-		
+
 		joinButton.draw(g);
 	}
-	
+
 	public void updateMouse(PointerEvent event) {
-		
 		createButton.handleEvent(event);
 		joinButton.handleEvent(event);
-		
-		if(event.isButtonDown(MouseButton.MOUSE_BUTTON_LEFT)) {
-			
-			if(createButton.isOnMouse()) {
-				
+
+		if (event.isButtonDown(MouseButton.MOUSE_BUTTON_LEFT)) {
+
+			if (createButton.isOnMouse()) {
 				LoungeMenu game = new LoungeMenu(w, h);
-				
+
 				NRobotServer server = new NRobotServer(mode);
 				server.start();
-				
-				NinjaRobotClient client =  new NinjaRobotClient(mode, game, "127.0.0.1");
+
+				NinjaRobotClient client = NinjaRobotClient.create(mode, game, "127.0.0.1");
 				client.setRole(NetworkRole.SERVER);
-				
+
 				session.put(MainMenu.PARAM_CLIENT, client);
 				session.put(MainMenu.PARAM_GAME, new GameState());
 				nextApplication = game;
 			}
-			
-			if(joinButton.isOnMouse()) {
-								
-				Config config = (Config)session.get(MainMenu.PARAM_CONFIG);
+
+			if (joinButton.isOnMouse()) {
+				Config config = (Config) session.get(MainMenu.PARAM_CONFIG);
 				String ip = config.getServerIp();
-				
+
 				LoungeMenu game = new LoungeMenu(w, h);
-				NinjaRobotClient client =  new NinjaRobotClient(mode, game, ip);
+				NinjaRobotClient client = NinjaRobotClient.create(mode, game, ip);
 				session.put(MainMenu.PARAM_CLIENT, client);
 				session.put(MainMenu.PARAM_GAME, new GameState());
 				nextApplication = game;
 			}
-			
 		}
 	}
 
@@ -97,5 +93,4 @@ public class RoomMenu extends Application {
 			nextApplication = new GameModeMenu(w, h);
 		}
 	}
-	
 }
--- src/br/com/nrobot/network/client/NinjaRobotClient.java
+++ src/br/com/nrobot/network/client/NinjaRobotClient.java
@@ -1,34 +1,32 @@
 package br.com.nrobot.network.client;
 
 import br.com.etyllica.core.event.KeyEvent;
+import br.com.midnight.client.TCPClient;
 import br.com.nrobot.game.GameMode;
 import br.com.nrobot.network.server.model.NetworkRole;
 
-public class NinjaRobotClient {
+public class NinjaRobotClient extends TCPClient {
 
 	public static final int BATTLE_PORT = 9967;
 	public static final int STORY_PORT = 9968;
 	private static final int CLIENT_DELAY = 100;
-	
+
 	private NRobotClientProtocol protocol;
-		
 	private NetworkRole role = NetworkRole.CLIENT;
-	
-	private NRobotClient client;
-	
-	public NinjaRobotClient(GameMode mode, NRobotClientListener listener, String ip) {
-		super();
-		
-		if(GameMode.BATTLE == mode) {
-			client = new NRobotClient(ip, BATTLE_PORT, listener);	
-		} else {
-			client = new NRobotClient(ip, STORY_PORT, listener);
-		}
-		
-		client.start(CLIENT_DELAY);
-		protocol = client.getActionProtocol();
+
+	public static NinjaRobotClient create(GameMode mode, NRobotClientListener listener, String ip) {
+		int port = mode == GameMode.BATTLE ? BATTLE_PORT : STORY_PORT;
+		return new NinjaRobotClient(ip, port, listener);
 	}
-	
+
+	public NinjaRobotClient(String ip, int port, NRobotClientListener listener) {
+		super(ip, port, true);
+
+		protocol = new NRobotClientProtocol(listener);
+		addProtocol(protocol);
+		start(CLIENT_DELAY);
+	}
+
 	public NRobotClientProtocol getProtocol() {
 		return protocol;
 	}
@@ -36,11 +34,11 @@ public class NinjaRobotClient {
 	public void setRole(NetworkRole role) {
 		this.role = role;
 	}
-	
+
 	public NetworkRole getRole() {
 		return role;
 	}
-	
+
 	public void handleEvent(KeyEvent event) {
 		handleKey(event, KeyEvent.VK_D, NRobotClientProtocol.KEY_RIGHT);
 		handleKey(event, KeyEvent.VK_RIGHT_ARROW, NRobotClientProtocol.KEY_RIGHT);
@@ -52,16 +50,16 @@ public class NinjaRobotClient {
 		handleKey(event, KeyEvent.VK_CTRL_LEFT, NRobotClientProtocol.KEY_JUMP);
 		handleKey(event, KeyEvent.VK_CTRL_RIGHT, NRobotClientProtocol.KEY_JUMP);
 	}
-	
+
 	private void handleKey(KeyEvent event, int keyCode, String keyText) {
-		if(event.isKeyDown(keyCode)) {
+		if (event.isKeyDown(keyCode)) {
 			protocol.sendPressKey(keyText);
-		} else if(event.isKeyUp(keyCode)) {
+		} else if (event.isKeyUp(keyCode)) {
 			protocol.sendReleaseKey(keyText);
 		}
 	}
-	
+
 	public void setListener(NRobotClientListener listener) {
-		client.getActionProtocol().setListener(listener);
+		protocol.setListener(listener);
 	}
 }
